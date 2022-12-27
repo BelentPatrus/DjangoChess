ffb2748b7798b9e248ddaf5e33b701c85952ed30
Matthew <matthew2898@hotmail.com>
2022-12-27 16:30:15 -0500

    Finished and tested Queen logic

--- mysite/chessengine/engine/Pieces/queen.py
+++ mysite/chessengine/engine/Pieces/queen.py
@@ -10,48 +10,88 @@ class Queen(Piece):
     def validMoves(self, board, position):
         moves = []
         row, col = position[0], position[1]
-        pieceType = board[row][col].type
+        pieceTeam = board[row][col].team.lower()
         i = 1
         # white: up, black: down
         print('In queen valid move')
-        while row-i >= 0 and (isinstance(board[row-i][col], Empty) or isinstance(board[row-i][col].type, Piece)):
+        while row-i >= 0 and board[row-i][col].team.lower() != pieceTeam:
+            print('piece team: {}, board piece team: {}'.format(board[row-i][col].team.lower(), pieceTeam))
             print('valid Move u:{}'.format([row-i,col]))
             moves.append((row-i, col))
+            if not isinstance(board[row-i][col], Empty):
+                break
             i+=1
+
         # white: down, black: up
         i = 1
-        while row+i < 8 and (isinstance(board[row+i][col], Empty) or isinstance(board[row+i][col], Piece)):
+        while row+i < 8 and board[row+i][col].team.lower() != pieceTeam:
+            print('piece team: {}, board piece team: {}'.format(board[row+i][col].team.lower(), pieceTeam))
             print('valid Move d :{}'.format([row+i,col]))
             moves.append((row+i,col))
+            if not isinstance(board[row+i][col], Empty):
+                break
             i+=1
         
+        i = 1
+        # white: right, black: left
+        while col+i < 8 and board[row][col+i].team.lower() != pieceTeam:
+            print('piece team: {}, board piece team: {}'.format(board[row][col+i].team.lower(), pieceTeam))
+            print('valid Move r :{}'.format([row,col+i]))
+            moves.append((row, col+i))
+            if not isinstance(board[row][col+i], Empty):
+                break
+            i+=1
+        
+        i = 1
+        # white: left, black: right
+        while col-i > 0 and board[row][col+i].team.lower() != pieceTeam:
+            print('piece team: {}, board piece team: {}'.format(board[row][col-i].team.lower(), pieceTeam))
+            print('valid Move r :{}'.format([row,col-i]))
+            moves.append((row, col-i))
+            if not isinstance(board[row][col-i], Empty):
+                break
+            i+=1
+
         # white: up-right diagonal, black: down-left diagonal
         i = 1
-        while row-i >= 0 and col+i < 8 and (isinstance(board[row-i][col+i], Empty) or isinstance(board[row-i][col+i], Piece)):
+        while row-i >= 0 and col+i < 8 and board[row-i][col+i].team.lower() != pieceTeam:
+            print('piece team: {}, board piece team: {}'.format(board[row-i][col+i].team.lower(), pieceTeam))
             print('valid Move up-r {}'.format([row-i,col+i]))
             moves.append((row-i,col+i))
+            if not isinstance(board[row-i][col+i], Empty):
+                break    
             i+=1
-        
+
         # white: up-left diagonal, black: down-right diagonal
         i = 1
-        while row-i >= 0 and col-i >= 0 and (isinstance(board[row-i][col-i], Empty) or isinstance(board[row-i][col-i], Piece)):
+        while row-i >= 0 and col-i >= 0 and board[row-i][col-i].team.lower() != pieceTeam:
+            print('piece team: {}, board piece team: {}'.format(board[row-i][col-i].team.lower(), pieceTeam))
             print('valid Move u-l: {}'.format([row-i,col-i]))
             moves.append((row-i,col-i))
+            if not isinstance(board[row-i][col-i], Empty):
+                break
             i+=1
         
         # white: down-right diagonal, black: up-left diagonal
         i = 1
-        while row+i < 8 and col+i < 8 and (isinstance(board[row+i][col+i], Empty) or isinstance(board[row+i][col+i], Piece)):
+        while row+i < 8 and col+i < 8 and board[row+i][col+i].team.lower() != pieceTeam:
+            print('board piece team: {}, piece team: {}'.format(board[row+i][col+i].team.lower(), pieceTeam))
             print('valid Move d-r: {}'.format([row+i,col+i]))
             moves.append((row+i,col+i))
+            if not isinstance(board[row+i][col+i], Empty):
+                break
             i+=1
-        
+
         # white: down-left diagonal, black: up-right diagonal
         i = 1
-        while row+i < 8 and col-i >=0 and (isinstance(board[row+i][col-i], Empty) or isinstance(board[row+i][col-i], Piece)):
+        while row+i < 8 and col-i >=0 and board[row+i][col-i].team.lower() != pieceTeam:
+            print('piece team: {}, board piece team: {}'.format(board[row+i][col-i].team.lower(), pieceTeam))
             print('valid Move d-l: {}'.format([row+i,col-i]))
             moves.append((row+i, col-i))
+            if not isinstance(board[row+i][col-i], Empty):
+                break
             i+=1
+
         
         return moves
         
--- mysite/chessengine/tests.py
+++ mysite/chessengine/tests.py
@@ -1,3 +1,79 @@
 from django.test import TestCase
-
+from chessengine.engine.chessboard import Chessboard as cb
 # Create your tests here.
+
+class testChessMoves(TestCase):
+    def testPawnMove(self):
+        wpm = {
+            'curr':[6,3],
+            'next': [4,3]
+        }
+        c = cb('White','Black')
+        wpawn = c.board[6][3]
+        c.movePiece(wpm)
+        self.assertTrue(c.board[4][3] == wpawn)
+        print('Done black pawn test 1')
+        wpm1 = {
+            'curr': [4,3],
+            'next': [3,3]
+        }
+
+        c.movePiece(wpm1)
+        self.assertTrue(c.board[3][3] == wpawn)
+        print('Done white pawn test 2')
+        bp = c.board[1][0]
+        bpm = {
+            'curr': [1,0],
+            'next': [2,0]
+        }
+        c.movePiece(bpm)
+        self.assertTrue(c.board[2][0] == bp)
+        print('Done black pawn test 1')
+        bpm1 = {
+            'curr': [2,0],
+            'next': [3,0]
+        }
+        c.movePiece(bpm1)
+        self.assertTrue(c.board[3][0] == bp)
+        print('Done black pawn test 2')
+
+    def testQueenMoves(self):
+        c = cb('White','Black')
+        wpm = {
+            'curr':[6,3],
+            'next': [4,3]
+        }
+        c.movePiece(wpm)
+        print('Finished moving pawn to 4,3')
+        q = c.board[7][3]
+        wqm = {
+            'curr':[7,3],
+            'next':[6,3]
+        }
+        c.movePiece(wqm)
+        self.assertTrue(c.board[6][3] == q)
+        print('Finished Queen moves test 1')
+        wqm1 = {
+            'curr':[6,3],
+            'next': [3,0]
+        }
+        c.movePiece(wqm1)
+        self.assertTrue(c.board[3][0] == q)
+        print('Finished Queen moves test 2')
+
+        # take move  test
+
+        wqm2= {
+            'curr':[3,0],
+            'next':[1,0]
+        }
+
+        c.movePiece(wqm2)
+        self.assertTrue(c.board[1][0] == q)
+        print('Done queen take test 3')
+        wqm3 = {
+            'curr':[1,0],
+            'next':[0,1]
+        }
+        c.movePiece(wqm3)
+        print('Done queen take test 4')
