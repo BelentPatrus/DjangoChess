ed24f54e594877d693016a73c7d319bf6f3ddfca
Matthew <matthew2898@hotmail.com>
2023-01-03 19:16:49 -0500

    Finished move highliter functionality

--- mysite/chessengine/engine/Pieces/bishop.py
+++ mysite/chessengine/engine/Pieces/bishop.py
@@ -15,7 +15,7 @@ class Bishop(Piece):
         while row-i >= 0 and col+i < 8 and board[row-i][col+i].team.lower() != pieceTeam:
             # print('piece team: {}, board piece team: {}'.format(board[row-i][col+i].team.lower(), pieceTeam))
             # print('valid Move up-r {}'.format([row-i,col+i]))
-            moves.append((row-i,col+i))
+            moves.append([row-i,col+i])
             if not isinstance(board[row-i][col+i], Empty):
                 break  
             i+=1
@@ -25,7 +25,7 @@ class Bishop(Piece):
         while row-i >= 0 and col-i >= 0 and board[row-i][col-i].team.lower() != pieceTeam:
             # print('piece team: {}, board piece team: {}'.format(board[row-i][col-i].team.lower(), pieceTeam))
             # print('valid Move u-l: {}'.format([row-i,col-i]))
-            moves.append((row-i,col-i))
+            moves.append([row-i,col-i])
             if not isinstance(board[row-i][col-i], Empty):
                 break
             i+=1
@@ -35,7 +35,7 @@ class Bishop(Piece):
         while row+i < 8 and col+i < 8 and board[row+i][col+i].team.lower() != pieceTeam:
             # print('board piece team: {}, piece team: {}'.format(board[row+i][col+i].team.lower(), pieceTeam))
             # print('valid Move d-r: {}'.format([row+i,col+i]))
-            moves.append((row+i,col+i))
+            moves.append([row+i,col+i])
             if not isinstance(board[row+i][col+i], Empty):
                 break
             i+=1
@@ -45,7 +45,7 @@ class Bishop(Piece):
         while row+i < 8 and col-i >=0 and board[row+i][col-i].team.lower() != pieceTeam:
             # print('piece team: {}, board piece team: {}'.format(board[row+i][col-i].team.lower(), pieceTeam))
             # print('valid Move d-l: {}'.format([row+i,col-i]))
-            moves.append((row+i, col-i))
+            moves.append([row+i, col-i])
             if not isinstance(board[row+i][col-i], Empty):
                 break
             i+=1
