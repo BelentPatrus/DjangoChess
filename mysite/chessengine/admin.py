5b0c76d9e7b1c38b1e0e77959a1ea34ad64ddb8c
Belent Patrus <Bpatr024@uottawa.ca>
2023-04-26 15:39:44 -0400

    chessboard model admin layout

--- mysite/chessengine/admin.py
+++ mysite/chessengine/admin.py
@@ -3,6 +3,16 @@ from .models import ChessboardModel, GameStateModel, ChessBoardMove
 # Register your models here.
 
 
-admin.site.register(ChessboardModel)
+# admin.site.register(ChessboardModel)
 admin.site.register(GameStateModel)
 admin.site.register(ChessBoardMove)
+
+
+@admin.display(description="GameState Id")
+def getGameStateId(obj):
+    return obj.id
+
+
+@admin.register(ChessboardModel)
+class chessboardModelAdmin(admin.ModelAdmin):
+    list_display = [getGameStateId, "playerTurn", "date"]
